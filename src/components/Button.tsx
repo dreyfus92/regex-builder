5470e7dd9ad732361a6f13aa08b0a417e2b32fea
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-04-15 10:03:59 -0600

    styles: changed button comp styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,17 @@
-import type { FC } from "react";
+import type { FC } from 'react'
 
 export interface ButtonProps
-  extends React.DetailedHTMLProps<
-      React.ButtonHTMLAttributes<HTMLButtonElement>,
-      HTMLButtonElement
-    >,
-    React.AriaAttributes {}
+	extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>,
+		React.AriaAttributes {}
 
 export const Button: FC<ButtonProps> = (props) => {
-  const { children, ...rest } = props;
-  return (
-    <button
-      {...rest}
-      className="flex items-center justify-center gap-2 rounded-xl border-4 border-black bg-red-700 px-8 py-4 font-bold shadow-[6px_6px_0_0_#000] transition hover:shadow-none focus:outline-none focus:ring active:bg-pink-50"
-    >
-      {children}
-    </button>
-  );
-};
+	const { children, ...rest } = props
+	return (
+		<button
+			{...rest}
+			className='flex items-center justify-center gap-2 rounded-xl border-4 border-black bg-red-700 px-8 py-4 font-bold shadow-[6px_6px_0_0_#000] transition hover:shadow-none focus:outline-none active:scale-95'
+		>
+			{children}
+		</button>
+	)
+}
