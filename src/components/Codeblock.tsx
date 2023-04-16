663b98242d8df8fd6f9c65a93eee0d438493a078
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-04-15 22:49:02 -0600

    chore: refactored Codeblock component and added TODO

--- src/components/Codeblock.tsx
+++ src/components/Codeblock.tsx
@@ -9,27 +9,42 @@ import { Button } from './Button'
 
 export type CodeblockProps = {
 	regex: string
+	test: string
 }
 
-export const Codeblock: FC<CodeblockProps> = ({ regex }) => {
+export const Codeblock: FC<CodeblockProps> = ({ regex, test }) => {
+	const [matches, setMatches] = useState<Array<string>>([])
+
 	const codeBlock = `
-	const regex  = ${regex}
-	const t = "abacus";
+	const regex = ${regex}
+	const t = ${test};
 	const matches  = t.match(regex);
+
+	// "print" is a special function that will log the results of your regex matches 
+	// to the textfield below the code editor
 	print(matches)
 	`
+
+	const testRegex = (regexRule: string, testSt: string) => {
+		const regex = new RegExp(regexRule)
+		const t = testSt
+		const matches = t.match(regex)
+
+		if (matches) {
+			setMatches(matches)
+		}
+	}
 
 	const [code, setCode] = useState(codeBlock)
 
 	return (
-		<div>
+		<div className='flex flex-col gap-4'>
 			<Editor
-				className=' rounded-xl p-4 w-[450px]'
+				className=' rounded-xl p-4 w-full md:w-[450px]'
 				tabSize={4}
 				value={code}
 				onValueChange={(code) => {
 					setCode(code)
-					console.log(code)
 				}}
 				highlight={(code) => Prism.highlight(code, Prism.languages.js, 'js')}
 				padding={10}
@@ -40,7 +55,20 @@ export const Codeblock: FC<CodeblockProps> = ({ regex }) => {
 					color: '#fff'
 				}}
 			/>
-			<Button onClick={() => console.log('clicked')}>Run</Button>
+			<Button onClick={() => testRegex(`${regex}`, test)}>Run</Button>
+			{matches && (
+				<div className='text-white'>
+					<ul>
+						<li>[</li>
+						{matches.map((match, i) => (
+							<li key={i} className='ml-4'>
+								"{match}"
+							</li>
+						))}
+						<li>]</li>
+					</ul>
+				</div>
+			)}
 		</div>
 	)
 }
--- src/components/RegexGen.tsx
+++ src/components/RegexGen.tsx
@@ -9,56 +9,63 @@ import { CopyToClipboard } from './CopyToClipboard'
 export const RegexGen: FC = () => {
 	const basePrompt = 'Write me a JavaScript regular expression that '
 	const [prompt, setPrompt] = useState<string>('')
+	const [testString, setTestString] = useState<string>('')
 
-	const getFinalPrompt = (basePrompt: string, prompt: string) => {
+	const getRegexFromOpenAI = (basePrompt: string, prompt: string) => {
 		if (basePrompt !== '' && prompt !== '') {
 			return basePrompt + prompt
 		} else {
-			return '/[a-z]/'
+			return '/[^a]/g'
 		}
 	}
 
-	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-		console.log(e.target.value)
+	const handlePromptChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setPrompt(e.target.value)
+	}
+
+	const handleTestStringChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setTestString(e.target.value)
 	}
 
 	return (
-		<div className='flex outline outline-red-400'>
+		<div className='flex flex-col my-10 gap-20 md:flex-row text-white'>
 			<div>
-				<div className='text-white'>
-					<p className='py-4'>Write me a JavaScript regular expression that…</p>
-					<div className='relative w-full min-w-[200px]'>
-						<textarea
-							className='peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-white border-t-white bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-indigo-500 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50'
-							placeholder=' '
-							onChange={handleChange}
-						></textarea>
-						<label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-indigo-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-indigo-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-indigo-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-							Write your prompt
-						</label>
+				<div>
+					<div>
+						<p className='py-4'>Write me a JavaScript regular expression that…</p>
+						<div className='relative w-full min-w-[200px]'>
+							<textarea
+								className='peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-white border-t-white bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-indigo-500 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50'
+								placeholder=' '
+								onChange={handlePromptChange}
+							></textarea>
+							<label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-indigo-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-indigo-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-indigo-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+								Write your prompt
+							</label>
+						</div>
 					</div>
-				</div>
-				<div className='text-white'>
-					<div className='flex gap-x-2 py-4'>
+
+					<div className='flex justify-between'>
 						<Button>Generate RegEx</Button>
-						<CopyToClipboard text={getFinalPrompt(basePrompt, prompt)} />
+						<CopyToClipboard text={getRegexFromOpenAI(basePrompt, prompt)} />
 					</div>
+
+					{/* TODO: Fix -> State is not being updated and passed into the Codeblock component */}
 					<div className='flex'>
-						<p>Test String</p>
 						<div className='relative h-10 w-full min-w-[200px]'>
 							<input
 								className='peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-white bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'
 								placeholder=' '
+								onChange={handleTestStringChange}
 							/>
 							<label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-								Input Medium
+								Test String
 							</label>
 						</div>
 					</div>
 				</div>
 			</div>
-			{/* Codeblock test */}
-			<Codeblock regex={getFinalPrompt(basePrompt, prompt)} />
+			<Codeblock regex={getRegexFromOpenAI(basePrompt, prompt)} test={testString} />
 		</div>
 	)
 }
